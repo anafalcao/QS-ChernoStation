be67a4f3fc7251914bd4225cdca3bdeb52dae122
luis-f-lins <luisfernandolins@gmail.com>
2019-12-01 17:30:26 -0300

    Remove comments from simulator

--- simulador/GerarDados.py
+++ simulador/GerarDados.py
@@ -2,24 +2,15 @@ import SimuladorDados
 import threading
 import random
 
-#Cria um nó virtual para simular uma unidade meteorológica que produzirá os dados
 def simular():
   while True:
     um = SimuladorDados.simulador_UM()
     um.main()
 
-n = 2 #Numero de threads/conexões com a cloud, maximo suportado é 30
+n = 2 
 for i in range(1,n):
     numero = str(i)
 
     t = threading.Thread(target=simular,args=())
     t.start()
     t.join()
-
-
-
-
-
-#
-# um01 = SimuladorDados.simulador_UM(50, 40, 50, "Cidade Universitaria", "01",1)
-# um01.main()
