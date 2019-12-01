e85b418f284f085db0d252b29703ef44ba57f80f
alexpsilva <alex.paivasilva@poli.ufrj.br>
2019-12-01 18:11:46 -0300

    Removido gráfico grande da tela de dashboard (pois estava redundante com os gráficos menores)

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,27 +1,8 @@
 import React from "react";
 import { useQuery } from '@apollo/react-hooks';
 
-import {
-  Grid,
-  Select,
-  MenuItem,
-  TextField,
-  InputLabel,
-  FormControl,
-  Button,
-  Paper,
-} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { useTheme } from "@material-ui/styles";
-import {
-  CartesianGrid,
-  Legend,
-  Tooltip,
-  ResponsiveContainer,
-  LineChart,
-  Line,
-  YAxis,
-  XAxis,
-} from "recharts";
 
 // styles
 import useStyles from "./styles";
@@ -32,26 +13,10 @@ import Widget from "../../components/Widget";
 import PageTitle from "../../components/PageTitle";
 import Table from "./components/Table/Table";
 import BigStat from "./components/BigStat/BigStat";
-import Queries from "../../queries";
 import Measurements from "../../models/Measurements";
 
 export default function Dashboard(props) {
   var classes = useStyles();
-  var theme = useTheme();
-
-  // form props
-  const [values, setValues] = React.useState({
-    medidor: '',
-    periodo: '',
-  });
-
-  const handleChange = event => {
-    setValues(oldValues => ({
-      ...oldValues,
-      [event.target.name]: event.target.value,
-    }));
-  };
-  
 
   // Getting the initial data for BigStats
   const measurement_types = useQuery(Measurements.GET_MEASUREMENT_TYPES).data;
@@ -73,118 +38,6 @@ export default function Dashboard(props) {
       <PageTitle title="Dashboard" />
       <Grid container spacing={4}>
         {bigStatGrid}
-        {/* Form */}
-        <Grid item xs={12}>
-        <Paper className={classes.formGrid}>
-            <form method="GET" action="controller" >
-            <input type="hidden"
-                   name="nomeDoTratadorDePagina"
-                   value="mvc.pagehandlers.Tratador_pagina2_jsp" />
-            <input type="hidden"
-                   name="form"
-                   value="form1" />
-            <table className={classes.mainFormTable}>
-              <tbody className={classes.mainFormBody}>
-                <tr className={classes.mainFormRow}>
-                    <td className={classes.mainFormItem}>
-                    <FormControl className={classes.formControl}>
-                      <InputLabel htmlFor="medidor-auto-width">Medidor</InputLabel>
-                      <Select
-                        value={values.medidor}
-                        onChange={handleChange}
-                        inputProps={{
-                          name: 'medidor',
-                          id: 'medidor-auto-width',
-                        }}
-                        autoWidth
-                      >
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
-                      </Select>
-                    </FormControl>
-                    </td>
-            <td id="datafinal" className={classes.mainFormItem}> 
-            <TextField
-              id="date"
-              label="Data final a ser exibida"
-              type="date"
-              defaultValue="2017-05-24"
-              className={classes.textField}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />  
-            </td>
-            <td className={classes.mainFormItem}>
-                    <FormControl className={classes.formControl}>
-                      <InputLabel htmlFor="medidor-auto-width">Período</InputLabel>
-                      <Select
-                        value={values.periodo}
-                        onChange={handleChange}
-                        inputProps={{
-                          name: 'periodo',
-                          id: 'periodo-auto-width',
-                        }}
-                        autoWidth
-                      >
-                        <MenuItem value={"ano"}>Ano</MenuItem>
-                        <MenuItem value={"mes"}>Mês</MenuItem>
-                        <MenuItem value={"semana"}>Semana</MenuItem>
-                        <MenuItem value={"dia"}>Dia</MenuItem>
-                      </Select>
-                    </FormControl>
-            </td>
-            
-                </tr>
-            
-                <tr className={classes.mainFormRow}>
-                  <td>
-                    <Button variant="outlined" onClick={Queries.getRulesByMeasurement}>
-                      Visualizar
-                    </Button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </form>
-          </Paper>
-        </Grid>
-        {/* Form */}
-        <Grid item xs={12}>
-        <Widget title="Gráfico" upperTitle>
-            <ResponsiveContainer width="100%" height={350}>
-              <LineChart
-                width={500}
-                height={300}
-                data={mock.lineChartData}
-                margin={{
-                  top: 20,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line
-                  type="monotone"
-                  dataKey="temperatura"
-                  stroke={theme.palette.primary.main}
-                  activeDot={{ r: 8 }}
-                />
-                <Line
-                  type="monotone"
-                  dataKey="pressao"
-                  stroke={theme.palette.secondary.main}
-                />
-              </LineChart>
-            </ResponsiveContainer>
-          </Widget>
-        </Grid>
         <Grid item xs={12}>
           <Widget
             title="Medidas mais recentes"
